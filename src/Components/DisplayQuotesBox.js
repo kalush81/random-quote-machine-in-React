4be626a31dd8880538be056ad4618f1703cd748c
kalush81 <krzysiek.kaluza@gmail.com>
2019-12-19 12:34:13 +0100

    Add DisplayQuotesBox component

---
+++ src/Components/DisplayQuotesBox.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export default class DisplayQuotesBox extends Component {
+  render() {
+    return (
+      <div className="main-box">
+        <div className="text-author-wrapper">
+          <h2 className="quote-text">
+            Life isn’t about getting and having, it’s about giving and being."
+          </h2>
+          <em className="quote-author">- author name</em>
+        </div>
+        <div className="footer-wrapper">
+          <div className="ikon-wrapper">
+            <i className="ikon">i</i>
+            <i className="ikon">i</i>
+          </div>
+          <button>new quote</button>
+        </div>
+      </div>
+    );
+  }
+}
