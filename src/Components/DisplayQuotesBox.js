b75561600971a7fe7f47e1a43e7901d400453a80
kalush81 <krzysiek.kaluza@gmail.com>
2019-12-20 09:38:48 +0100

    Get random quote on click

--- src/Components/DisplayQuotesBox.js
+++ src/Components/DisplayQuotesBox.js
@@ -1,23 +1,48 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class DisplayQuotesBox extends Component {
+class DisplayQuotesBox extends Component {
+  state = {
+    idx: 0
+  };
+
+  setRandIndex = () => {
+    this.setState({
+      idx: Math.floor(Math.random() * (this.props.quotes.length - 0 + 1) + 0)
+    });
+  };
+  componentWillReceiveProps() {
+      this.props.quotes && this.setState({idx: 10})
+  }
   render() {
+    const { quotes } = this.props;
+    const { idx } = this.state;
+    
     return (
-      <div className="main-box">
-        <div className="text-author-wrapper">
-          <h2 className="quote-text">
-            Life isn’t about getting and having, it’s about giving and being."
-          </h2>
-          <em className="quote-author">- author name</em>
-        </div>
-        <div className="footer-wrapper">
-          <div className="ikon-wrapper">
-            <i className="ikon">i</i>
-            <i className="ikon">i</i>
+      <div>
+          {console.log('quotes in render',quotes)}
+        {quotes && quotes.length > 0 ?(
+          <div className="main-box">
+            <div className="text-author-wrapper">
+              <h2 className="quote-text">{quotes[idx].quote}</h2>
+              <em className="quote-author">- {quotes[idx].author}</em>
+            </div>
+            <div className="footer-wrapper">
+              <div className="ikon-wrapper">
+                <i className="ikon">i</i>
+                <i className="ikon">i</i>
+              </div>
+              <button onClick={this.setRandIndex}>get new quote</button>
+            </div>
           </div>
-          <button>new quote</button>
-        </div>
+        ) : '...load'}
       </div>
     );
   }
 }
+function mapStateToProps(state) {
+  return {
+    quotes: state.quotes
+  };
+}
+export default connect(mapStateToProps, null)(DisplayQuotesBox);
